670bf547ec39c5178d6a0169143895c47f3f33c7
uli.herwig <uli-herwig@t-online.de>
2025-02-17 16:46:20 +0100

    2025.02.2 try-catch added

--- Core/SharedKernel/Extensions/AuthenticationExtension.cs
+++ Core/SharedKernel/Extensions/AuthenticationExtension.cs
@@ -4,30 +4,37 @@ public static class AuthenticationExtension
 {
     public static IServiceCollection AddKeyCloakAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddAuthentication(options =>
+        try
         {
-            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-        .AddJwtBearer(options =>
-        {
-            options.Authority = $"{configuration["Keycloak:Authority"]}";
-            options.MetadataAddress = $"{configuration["Keycloak:Host"]}/realms/{configuration["Keycloak:Realm"]}/.well-known/openid-configuration";
-            options.Audience = configuration["Keycloak:Realm"];
-            options.RequireHttpsMetadata = false;
-            options.TokenValidationParameters = new TokenValidationParameters
+            services.AddAuthentication(options =>
             {
-                NameClaimType = ClaimTypes.Name,
-                RoleClaimType = "roles",
-                ValidateIssuer = true,
-                ValidIssuers = new[] { $"{configuration["Keycloak:Authority"]}" },
-                ValidateAudience = true,
-                ValidAudiences = new[] { "account" },
-                AuthenticationType = "Bearer"
-            };
-        });
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.Authority = $"{configuration["Keycloak:Authority"]}";
+                options.MetadataAddress = $"{configuration["Keycloak:Host"]}/realms/{configuration["Keycloak:Realm"]}/.well-known/openid-configuration";
+                options.Audience = configuration["Keycloak:Realm"];
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    NameClaimType = ClaimTypes.Name,
+                    RoleClaimType = "roles",
+                    ValidateIssuer = true,
+                    ValidIssuers = new[] { $"{configuration["Keycloak:Authority"]}" },
+                    ValidateAudience = true,
+                    ValidAudiences = new[] { "account" },
+                    AuthenticationType = "Bearer"
+                };
+            });
 
-        services.AddAuthorization();
+            services.AddAuthorization();
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error configuring Keycloak authentication", ex);
+        }
 
         return services;
     }
